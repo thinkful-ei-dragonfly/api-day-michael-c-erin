cff56f00bc0f648d71cba3c8e9277c34c076cabd
m-cal <iengineersoftware@gmail.com>
2019-05-08 14:51:16 -0400

    finished third step

--- scripts/api.js
+++ scripts/api.js
@@ -4,13 +4,6 @@ let api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/michaelc';
   let getItems = function() {
     return fetch(`${BASE_URL}/items`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(response.statusText);
-      }); 
-      
   };
 
   let createItem = function(name) {
